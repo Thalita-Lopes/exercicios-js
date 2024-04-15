ba2c0f058f2b056bd4c9b6413e656431f0a44b3b
Thalita <thalimascarenhas@gmail.com>
2024-04-15 20:37:03 -0300

    números

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,8 @@
+console.log(7 / 0) // = Infinity 
+console.log("10" / 2)
+console.log("Show!" * 2 ) // = NaN
+console.log(0.1 + 0.7) // = 0.7999999999
+// console.log(10.toSring()) -> não funciona
+console.log((10.345).toFixed(2)) // Funciona se o número estiver entre parenteses
+let a = (10).toString()
+console.log(typeof a)
---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,15 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar nomes genéricos e siglas
+let valor = 3.25
+let numero = 1
+let vlw = false
+
+// Prefira código claro a comentário de código
+// Use mais a variável 'let'
