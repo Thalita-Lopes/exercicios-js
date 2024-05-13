6808d043ac5651bbdd56c3be367c04beac45254d
Thalita <thalimascarenhas@gmail.com>
2024-05-13 14:15:42 -0300

    Dias de estudo: 8

---
+++ funcao/factory1.js
@@ -0,0 +1,11 @@
+// Factory simples
+function criarPessoa() {
+    return {
+        nome: 'Ana',
+        sobrenome: 'Silva'
+    }
+}
+
+console.log(criarPessoa())
+
+// Uma função factory é uma função que retorna um objeto
