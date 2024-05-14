da33818bdeaa21863c10b30758a2c05abff9cda3
Thalita <thalimascarenhas@gmail.com>
2024-05-14 15:26:14 -0300

    Lista de Exercícios

---
+++ listaExs/ex01.js
@@ -0,0 +1,8 @@
+function operacoes(x, y) {
+    console.log(`soma: ${x} + ${y} = ${x + y}`)
+    console.log(`subtração: ${x} - ${y} = ${x - y}`)
+    console.log(`multiplicação: ${x} x ${y} = ${x * y}`)
+    console.log(`divisão: ${x} / ${y} = ${x / y}`)
+}
+
+operacoes(2, 5)
