6384d3c5df23dc9be20d4f06fa3fb57ad9c1c0aa
Thalita <thalimascarenhas@gmail.com>
2024-06-03 16:30:48 -0300

    Exercício 10

---
+++ listaExs/ex10.js
@@ -0,0 +1,12 @@
+const divisivelPor3 = function (num) {
+    if (num%3 == 0){
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(divisivelPor3(4))
+console.log(divisivelPor3(6))
+console.log(divisivelPor3(2))
+console.log(divisivelPor3(36))
