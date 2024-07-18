77562be5878a175ee6c07e4f60cd42fa12ff25ea
Thalita <thalimascarenhas@gmail.com>
2024-07-18 11:12:38 -0300

    Exercício 18

---
+++ listaExs/ex18.js
@@ -0,0 +1,50 @@
+function EscreveNumeroPorExtenso(numero) {
+    switch(numero){
+        case 1:
+            console.log('Um')
+            break
+        case 2:
+            console.log('Dois')
+            break
+        case 3:
+            console.log('Três')
+            break
+        case 4:
+            console.log('Quatro')
+            break
+        case 5:
+            console.log('Cinco')
+            break
+        case 6:
+            console.log('Seis')
+            break
+        case 7:
+            console.log('Sete')
+            break
+        case 8:
+            console.log('Oito')
+            break
+        case 9:
+            console.log('Nove')
+            break
+        case 10:
+            console.log('Dez')
+            break
+        default:
+            console.log('Número fora do intervalo.')
+    }
+}
+
+EscreveNumeroPorExtenso(1)
+EscreveNumeroPorExtenso(2)
+EscreveNumeroPorExtenso(3)
+EscreveNumeroPorExtenso(4)
+EscreveNumeroPorExtenso(5)
+EscreveNumeroPorExtenso(6)
+EscreveNumeroPorExtenso(7)
+EscreveNumeroPorExtenso(8)
+EscreveNumeroPorExtenso(9)
+EscreveNumeroPorExtenso(10)
+EscreveNumeroPorExtenso(11)
+EscreveNumeroPorExtenso(12)
+EscreveNumeroPorExtenso(13)
